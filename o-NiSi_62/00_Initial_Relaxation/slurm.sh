ab3496b05222b7acf4a8f5e682cd8a7fe64ce603
dinurah95 <dhettiarachchi95@gmail.com>
2025-12-27 23:39:38 -0500

    62_NiSi_VASP

---
+++ o-NiSi_62/00_Initial_Relaxation/slurm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=NiSi_62_InitialRelaxation
+#SBATCH --account=def-chihang
+#SBATCH --ntasks-per-node=48    # number of MPI processes per node
+#SBATCH --nodes=2              # number of nodes
+#SBATCH --mem-per-cpu=10G       # memory
+#SBATCH --time=0-05:00         # time (DD-HH:MM)
+#SBATCH --output=vasp.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=dhett034@uottawa.ca
+
+
+echo '-------------------Job starts at-------------------'
+date "+%k:%M:%S %b-%d-%Y"
+echo '---------------------------------------------------'
+
+
+module load StdEnv/2023 intel/2023.2.1 intelmpi/2021.9.0
+module load vasp/6.4.2
+srun vasp_std
+
+
+echo '--------------------Job ends at--------------------'
+date "+%k:%M:%S %b-%d-%Y"
+echo '---------------------------------------------------'
