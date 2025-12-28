102eb83c7f6b774c817964213917961e45c88981
dinurah95 <dhettiarachchi95@gmail.com>
2025-12-27 23:42:36 -0500

    59_NiSi_VASP

---
+++ o-NiSi_59/convergence/ENCUT/slurm.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#SBATCH --job-name=NiSi_ENCUT_ConvergenceRun
+#SBATCH --account=def-chihang
+#SBATCH --ntasks-per-node=32    # number of MPI processes per node
+#SBATCH --nodes=1              # number of nodes
+#SBATCH --mem-per-cpu=3G       # memory
+#SBATCH --time=0-01:00         # time (DD-HH:MM)
+#SBATCH --output=vasp.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=dhett034@uottawa.ca
+
+echo '-------------------Job starts at-------------------'
+date "+%k:%M:%S %b-%d-%Y"
+echo '---------------------------------------------------'
+
+# Load required modules
+module load StdEnv/2023 intel/2023.2.1 openmpi/4.1.5
+module load vasp/6.4.2
+
+# Loop over the ENCUT values (200, 230, 260, ..., 520)
+for encut in {200..530..30}
+do
+    # Create a directory for each ENCUT value
+    mkdir -p ENCUT_$encut
+    cp INCAR KPOINTS POSCAR POTCAR ENCUT_$encut/
+
+    # Modify the INCUT value in the INCAR file for this loop iteration
+    sed -i "s/ENCUT = .*/ENCUT = $encut/" ENCUT_$encut/INCAR
+
+    # Change to the ENCUT-specific directory and run the job
+    cd ENCUT_$encut
+
+    # Run VASP Job
+    echo "Running VASP for ENCUT = $encut"
+    srun vasp_std
+
+    # Return to the original directory
+    cd ..
+done
+
+echo '--------------------Job ends at--------------------'
+date "+%k:%M:%S %b-%d-%Y"
+echo '---------------------------------------------------'
+
---
+++ o-NiSi_59/convergence/KPOINTS/slurm.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH --job-name=NiSi_KPOINTS_ConvergenceTest
+#SBATCH --account=def-chihang
+#SBATCH --ntasks-per-node=32    # number of MPI processes per node
+#SBATCH --nodes=1              # number of nodes
+#SBATCH --mem-per-cpu=3G       # memory
+#SBATCH --time=0-03:00         # time (DD-HH:MM)
+#SBATCH --output=vasp.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=dhett034@uottawa.ca
+
+echo '-------------------Job starts at-------------------'
+date "+%k:%M:%S %b-%d-%Y"
+echo '---------------------------------------------------'
+
+# Load required modules
+module load StdEnv/2023 intel/2023.2.1 openmpi/4.1.5
+module load vasp/6.4.2
+
+# Loop over the KPOINTS values (4 4 4, 6 6 6, ..., 20 20 20)
+for k in {4..12..2}
+do
+    # Create a directory for each KPOINTS value
+    mkdir -p KPOINTS_$k
+    cp INCAR POSCAR POTCAR KPOINTS_$k/
+
+    # Modify the KPOINTS file for this loop iteration
+    echo -e "Automatic mesh\n0\nGamma\n$k $k $k\n0\n" > KPOINTS_$k/KPOINTS
+
+    # Change to the KPOINTS-specific directory
+    cd KPOINTS_$k
+
+    # Run the VASP job
+    echo "Running VASP for KPOINTS = $k $k $k"
+    srun vasp_std
+
+    # Return to the original directory
+    cd ..
+done
+
+echo '--------------------Job ends at--------------------'
+date "+%k:%M:%S %b-%d-%Y"
+echo '---------------------------------------------------'
